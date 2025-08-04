3f500e6655c9df1aa29f24b0d1ddaacb2991cbce
TheLuaFox86 <peanuts005@outlook.com>
2025-08-04 00:07:58 +0000

    poop

--- LUA/terminal.lua
+++ LUA/terminal.lua
@@ -5,21 +5,10 @@ js.global.document:write([[
 local entered = false
 local ent = js.global.document:getElementById("enter")
 local dsp = js.global.document:getElementById("DSP")
-js.global:alert(dsp)
 _G.term = {
 	read = function()
-		local c = ""
-		local a = function()
-			c = js.global.window:prompt("input")
-			entered = true
-		end
-		ent.onclick = a
-		repeat
-			--Wait Until
-		until entered
-		entered = false
 		ent.onclick = function()
-			return;
+			
 		end
 		return c
 	end,
